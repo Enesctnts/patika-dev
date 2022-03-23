ec6e0fd9448ed0a1aa0df4c58f1549c023b64c56
Enes Çetintaş <enesctnts@gmail.com>
2022-03-23 21:44:34 +0300

    Update Program.cs

--- Koleksiyonlar-Soru-1/Program.cs
+++ Koleksiyonlar-Soru-1/Program.cs
@@ -100,10 +100,3 @@ namespace Koleksiyonlar_Soru_1
     }
 
 }
-/*
-Klavyeden girilen 20 adet pozitif sayının asal ve asal olmayan olarak 2 ayrı listeye atın. (ArrayList sınıfını kullanara yazınız.)
-
-Negatif ve numeric olmayan girişleri engelleyin.
-Her bir dizinin elemanlarını büyükten küçüğe olacak şekilde ekrana yazdırın.
-Her iki dizinin eleman sayısını ve ortalamasını ekrana yazdırın.
-*/
